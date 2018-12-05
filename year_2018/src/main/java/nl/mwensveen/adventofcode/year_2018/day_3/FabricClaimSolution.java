3a39f8b148e244e2b2e1f73045914d9f79092dc1
mwensveen <mail@mwensveen.nl>
2018-12-05 13:44:42 +0100

    add stopwatch for day 3

--- year_2018/src/main/java/nl/mwensveen/adventofcode/year_2018/day_3/FabricClaimSolution.java
+++ year_2018/src/main/java/nl/mwensveen/adventofcode/year_2018/day_3/FabricClaimSolution.java
@@ -6,18 +6,24 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 
 public class FabricClaimSolution {
 
     public static void main(String[] args) throws IOException {
+        Instant start = Instant.now();
+
         URL url = Resources.getResource("Day3.txt");
         List<String> lines = Files.readLines(new File(url.getFile()), Charset.defaultCharset());
 
         FabricClaimAnalyser analyser = new FabricClaimAnalyser(lines);
 
         System.out.println("Overlapping squares: " + analyser.calculateOverlap());
+        System.out.println(Duration.between(start, Instant.now()).toMillis());
         System.out.println("Claim without overlap: " + analyser.findClaimWithoutOverlap());
+        System.out.println(Duration.between(start, Instant.now()).toMillis());
 
     }
 
