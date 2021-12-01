e3f3a4b64cf0af08a350ff0d41e8f1096dc8e19a
mwensveen-nl <mail@mwensveen.nl>
2021-12-01 16:28:57 +0100

    2021 - Day 01 - small improvements

--- year_2021/src/main/java/nl/mwensveen/adventofcode/year_2021/day_01/DepthMeasurementCalculator.java
+++ year_2021/src/main/java/nl/mwensveen/adventofcode/year_2021/day_01/DepthMeasurementCalculator.java
@@ -9,16 +9,13 @@ public class DepthMeasurementCalculator {
 
     long calculateNumberOfIncreases(List<Integer> depths) {
         Stream<Integer> stream = depths.stream();
-        return StreamUtils.zipWithIndex(stream).map(di -> {
-            if (di.getIndex() > 0) {
-                return (di.getValue() > depths.get(((int) di.getIndex()) - 1));
-            }
-            return false;
+        return StreamUtils.zipWithIndex(stream).skip(1).map(di -> {
+            return (di.getValue() > depths.get(((int) di.getIndex()) - 1));
         }).filter(b -> b).count();
     }
 
     long calculateNumberOfIncreasesSumMeasurements(List<Integer> depths) {
-        List<Integer> summedDepths = StreamUtils.skipWhile(StreamUtils.zipWithIndex(depths.stream()), di -> di.getIndex() < 2)
+        List<Integer> summedDepths = StreamUtils.zipWithIndex(depths.stream()).skip(2)
                 .map(di -> di.getValue() + depths.get((int) di.getIndex() - 1) + depths.get((int) di.getIndex() - 2)).collect(Collectors.toList());
 
         return calculateNumberOfIncreases(summedDepths);
