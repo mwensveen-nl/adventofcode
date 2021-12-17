e2fbc744891ebaf5aa8388019e9426e591a76d6c
Micha Wensveen <mail@mwensveen.nl>
2021-12-17 09:05:15 +0100

    2021 - day 15 part 2 improve with priority queue

--- year_2021/src/main/java/nl/mwensveen/adventofcode/year_2021/day_15/RiskPathFinder.java
+++ year_2021/src/main/java/nl/mwensveen/adventofcode/year_2021/day_15/RiskPathFinder.java
@@ -1,11 +1,10 @@
 package nl.mwensveen.adventofcode.year_2021.day_15;
 
 import com.google.common.collect.Table;
-import java.util.Collection;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class RiskPathFinder {
 
@@ -13,20 +12,22 @@ public class RiskPathFinder {
         RiskLevelParser parser = new RiskLevelParser();
         Table<Integer, Integer, RiskLevel> cave = parser.parseInput(input, times);
         RiskLevel last = cave.get(cave.rowKeySet().size() - 1, cave.columnKeySet().size() - 1);
-
-        Collection<RiskLevel> unvisited = new HashSet<>(cave.values());
-        Optional<RiskLevel> current = unvisited.stream().min(Comparator.comparingLong(rl -> rl.getDistance()));
-        while (current.isPresent()) {
-            processCurrent(current.get(), cave);
-            unvisited.remove(current.get());
-            current = unvisited.stream().min(Comparator.comparingLong(rl -> rl.getDistance()));
+        Queue<RiskLevel> unvisited = new PriorityQueue<>(Comparator.comparingLong(rl -> rl.getDistance()));
+        unvisited.add(cave.get(0, 0));
+        RiskLevel current = unvisited.poll();
+        while (current != null) {
+            processCurrent(current, cave, unvisited);
+            current = unvisited.poll();
         }
 
         return last.getDistance();
     }
 
-    private void processCurrent(RiskLevel current, Table<Integer, Integer, RiskLevel> cave) {
-        current.notVisitedAdjents().forEach(a -> setDistance(a, current.getDistance()));
+    private void processCurrent(RiskLevel current, Table<Integer, Integer, RiskLevel> cave, Queue<RiskLevel> unvisited) {
+        List<RiskLevel> adjents = current.notVisitedAdjents();
+        adjents.stream().filter(rl -> rl.getDistance() != Long.MAX_VALUE).forEach(rl -> unvisited.remove(rl));
+        adjents.forEach(a -> setDistance(a, current.getDistance()));
+        adjents.forEach(rl -> unvisited.add(rl));
         current.setVisited(true);
     }
 
